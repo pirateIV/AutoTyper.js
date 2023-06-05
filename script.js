f472a713788a49b03b019e44106171572f801017
Benjamin Oluwadarasimi <benabolade@gmail.com>
2023-06-05 07:47:43 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 
 const output = document.getElementById('output')
-const stringArr = ['Benjamin Abolade', 'ChatOpenAI or ChatGPT', 'Search Google to find the words that you are looking for']
+const stringArr = ['Benjamin Abolade', '', 'Search Google to find what you are looking for']
 
 let index = 0;
 let currentIndex = -1;
+let interval;
 
 function autotype() {
-  setInterval(() => {
+  interval = setInterval(() => {
     currentIndex++
 
-    output.innerHTML += stringArr[index][currentIndex]
-
-    if(currentIndex > stringArr[index].length - 1) {
-      console.log('Reached the initial stage')
-      output.innerHTML = ''
-      currentIndex = -1
-      index++
-      
+    if(currentIndex <= stringArr[index].length - 1){
+      output.innerHTML += stringArr[index][currentIndex]
+    } else {
+      clearInterval(interval)
+      setTimeout(() => {
+        reverseString()
+      }, 1000)
     }
-    // if(index > stringArr.length) {
-    //     index--
-    //     autotype()
-    // }
+
   }, 100)
 }
 
 function reverseString(){
-
+  interval = setInterval(() => {
+    if(currentIndex >= 0){
+      output.innerHTML = output.innerHTML.slice(0, -1)
+      currentIndex--
+    } else {
+      clearInterval(interval)
+      index++
+      // index = (index + 1) % stringArr.length - Restart from the beginning
+      autotype()
+    }
+  }, 100)
 }
 
 autotype()
